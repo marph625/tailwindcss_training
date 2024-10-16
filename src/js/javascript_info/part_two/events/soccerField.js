80968344ebe00315ff0c0542ac007b16a4cba5e9
marph625 <marvin.gnesner@gmail.com>
2024-10-16 14:16:14 +0200

    soccerField: The ball moves to the right position TODO: boundaries and the first click is weird -> starting position

--- src/js/javascript_info/part_two/events/soccerField.js
+++ src/js/javascript_info/part_two/events/soccerField.js
@@ -4,14 +4,19 @@ document.addEventListener("click", () => {
         // ball.event.clientX = event.clientX;
         // ball.event.clientY = event.clientY;
 
-        let x_pos = event.clientX;
-        let y_pos = event.clientY;
+        let ballSize = 40;
+
+        let x_pos = event.clientX - ballSize/2;
+        let y_pos = event.clientY - ballSize/2;
 
         let ball = document.getElementById("ball");
 
         ball.style.position = "absolute";
         ball.style.left = x_pos+'px';
         ball.style.top = y_pos+'px';
-        console.log("Clicked")
+        ball.style.transition = 1000 +'ms';
+        console.log("Coordinates Cursor: " + event.clientX + " : " + event.clientY);
+        console.log("Coordinates Ball: " + ball.style.left + " : " + ball.style.top);
+        console.log("---------------------------");
     }
 })
