111d5d07065d2e94e2d4ab4d3ba8a6eed37b1bc3
marph625 <marvin.gnesner@gmail.com>
2024-10-16 15:18:25 +0200

    soccerField: boundaries and movement done. The first click is weird -> starting position

--- src/js/javascript_info/part_two/events/soccerField.js
+++ src/js/javascript_info/part_two/events/soccerField.js
@@ -1,22 +1,50 @@
 document.addEventListener("click", () => {
     field.onclick = (event) => {
-        // alert(event.clientX + " : " + event.clientY)
-        // ball.event.clientX = event.clientX;
-        // ball.event.clientY = event.clientY;
-
+        // size of the ball-image in pixels
         let ballSize = 40;
 
-        let x_pos = event.clientX - ballSize/2;
-        let y_pos = event.clientY - ballSize/2;
+        // min-max range for x and y coordinates
+        let xMin = 32;
+        let xMax = 232;
+        let yMin = 72;
+        let yMax = 208;
 
+        // store the mouse position in variables
+        let ballImgX = event.clientX;
+        let ballImgY = event.clientY;
+
+        // I want the center of the image move to the coordinates, not the upper-left edge
+        let x_pos = ballImgX - ballSize/2;
+        let y_pos = ballImgY - ballSize/2;
+
+        // get the div and store it so I can use it
         let ball = document.getElementById("ball");
 
         ball.style.position = "absolute";
-        ball.style.left = x_pos+'px';
-        ball.style.top = y_pos+'px';
+
+
+        // boundary logic
+        if (x_pos > xMax) {
+            ball.style.left = xMax+'px';
+        } else if (x_pos < xMin) {
+            ball.style.left = xMin+'px';
+        } else {
+            ball.style.left = x_pos+'px';
+        }
+
+        if (y_pos > yMax) {
+            ball.style.top = yMax+'px';
+        } else if (y_pos < yMin) {
+            ball.style.top = yMin+'px';
+        } else {
+            ball.style.top = y_pos+'px';
+        }
+
         ball.style.transition = 1000 +'ms';
-        console.log("Coordinates Cursor: " + event.clientX + " : " + event.clientY);
-        console.log("Coordinates Ball: " + ball.style.left + " : " + ball.style.top);
+
+        // Console output for debugging
+        console.log("Coordinates Cursor X: " + event.clientX + " | Y: " + event.clientY);
+        console.log("Coordinates Ball Center X: " + ballImgX + " | Y: " + ballImgY);
         console.log("---------------------------");
     }
 })
