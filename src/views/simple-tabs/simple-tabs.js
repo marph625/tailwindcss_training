910cb4322a85fd9da006188e5277ba209c54ea1c
marph625 <marvin.gnesner@gmail.com>
2024-10-22 16:48:31 +0200

    project simple-tabs: improvements (loop instead of huge if-else)

--- src/views/simple-tabs/simple-tabs.js
+++ src/views/simple-tabs/simple-tabs.js
@@ -2,55 +2,79 @@ document.addEventListener('DOMContentLoaded', function() {
     let dynamicTabs = document.querySelectorAll('.tab-content');
     let tabSelectors = document.querySelectorAll('.tab-selector');
 
-    document.addEventListener('click', function(event) {
-
-            let tab = event.target.closest('.tab-selector')
-            if (tab === tabSelectors[0]) {
-                dynamicTabs[0].classList.add('flex');
-                dynamicTabs[0].classList.remove('hidden');
-                dynamicTabs[1].classList.add('hidden');
-                dynamicTabs[2].classList.add('hidden');
-                dynamicTabs[3].classList.add('hidden');
-
-                tabSelectors[0].classList.add('font-bold');
-                tabSelectors[1].classList.remove('font-bold');
-                tabSelectors[2].classList.remove('font-bold');
-                tabSelectors[3].classList.remove('font-bold');
-            } else if (tab === tabSelectors[1]) {
-                dynamicTabs[1].classList.add('flex');
-                dynamicTabs[1].classList.remove('hidden');
-                dynamicTabs[0].classList.add('hidden');
-                dynamicTabs[2].classList.add('hidden');
-                dynamicTabs[3].classList.add('hidden');
-
-                tabSelectors[1].classList.add('font-bold');
-                tabSelectors[0].classList.remove('font-bold');
-                tabSelectors[2].classList.remove('font-bold');
-                tabSelectors[3].classList.remove('font-bold');
-            } else if (tab === tabSelectors[2]) {
-                dynamicTabs[2].classList.add('flex');
-                dynamicTabs[2].classList.remove('hidden');
-                dynamicTabs[0].classList.add('hidden');
-                dynamicTabs[1].classList.add('hidden');
-                dynamicTabs[3].classList.add('hidden');
-
-                tabSelectors[2].classList.add('font-bold');
-                tabSelectors[0].classList.remove('font-bold');
-                tabSelectors[1].classList.remove('font-bold');
-                tabSelectors[3].classList.remove('font-bold');
-
+    function loopyApproach(index) {
+        dynamicTabs.forEach((tabContent, i) => {
+            if (i === index) {
+                tabContent.classList.add('flex');
+                tabContent.classList.remove('hidden');
+                tabSelectors[i].classList.add('font-bold');
             } else {
-                dynamicTabs[3].classList.add('flex');
-                dynamicTabs[3].classList.remove('hidden');
-                dynamicTabs[0].classList.add('hidden');
-                dynamicTabs[1].classList.add('hidden');
-                dynamicTabs[2].classList.add('hidden');
-
-                tabSelectors[3].classList.add('font-bold');
-                tabSelectors[0].classList.remove('font-bold');
-                tabSelectors[2].classList.remove('font-bold');
-                tabSelectors[1].classList.remove('font-bold');
+                tabContent.classList.add('hidden');
+                tabContent.classList.remove('flex');
+                tabSelectors[i].classList.remove('font-bold');
             }
+        });
+    }
 
+    loopyApproach(0);
+
+    tabSelectors.forEach((tab, index) => {
+        tab.addEventListener('click', () => loopyApproach(index))
     })
+
+    // document.addEventListener('click', function(event) {
+    //
+    //         bad_if_else(dynamicTabs, tabSelectors, event);
+    // })
 })
+
+function bad_if_else(tabs, selectors, event) {
+    let tab = event.target.closest('.tab-selector')
+    if (tab === selectors[0]) {
+        tabs[0].classList.add('flex');
+        tabs[0].classList.remove('hidden');
+        tabs[1].classList.add('hidden');
+        tabs[2].classList.add('hidden');
+        tabs[3].classList.add('hidden');
+
+        selectors[0].classList.add('font-bold');
+        selectors[1].classList.remove('font-bold');
+        selectors[2].classList.remove('font-bold');
+        selectors[3].classList.remove('font-bold');
+    } else if (tab === selectors[1]) {
+        tabs[1].classList.add('flex');
+        tabs[1].classList.remove('hidden');
+        tabs[0].classList.add('hidden');
+        tabs[2].classList.add('hidden');
+        tabs[3].classList.add('hidden');
+
+        selectors[1].classList.add('font-bold');
+        selectors[0].classList.remove('font-bold');
+        selectors[2].classList.remove('font-bold');
+        selectors[3].classList.remove('font-bold');
+    } else if (tab === selectors[2]) {
+        tabs[2].classList.add('flex');
+        tabs[2].classList.remove('hidden');
+        tabs[0].classList.add('hidden');
+        tabs[1].classList.add('hidden');
+        tabs[3].classList.add('hidden');
+
+        selectors[2].classList.add('font-bold');
+        selectors[0].classList.remove('font-bold');
+        selectors[1].classList.remove('font-bold');
+        selectors[3].classList.remove('font-bold');
+
+    } else {
+        tabs[3].classList.add('flex');
+        tabs[3].classList.remove('hidden');
+        tabs[0].classList.add('hidden');
+        tabs[1].classList.add('hidden');
+        tabs[2].classList.add('hidden');
+
+        selectors[3].classList.add('font-bold');
+        selectors[0].classList.remove('font-bold');
+        selectors[2].classList.remove('font-bold');
+        selectors[1].classList.remove('font-bold');
+    }
+}
+
