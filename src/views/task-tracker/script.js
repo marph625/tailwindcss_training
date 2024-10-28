b556676c883816904e073193b355e5865e4e8c78
marph625 <marvin.gnesner@gmail.com>
2024-10-28 16:42:03 +0100

    improves styling of task-tracker

--- src/views/task-tracker/script.js
+++ src/views/task-tracker/script.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputButton = document.getElementById('input-button');
     const taskContainer = document.createElement('div');
     const parentDiv = document.getElementById('parent-div');
+
+    const openTasks = [];
+    const doneTasks = [];
     
     let taskId = 0;
     
@@ -29,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const newCheckbox = document.createElement('input');
         newCheckbox.type = "checkbox";
         newCheckbox.id = "task" + taskId++;
-        newCheckbox.classList.add('mt-2');
+        newCheckbox.classList.add('mt-2', 'border-2', 'border-black');
         
         const taskLabel = document.createElement('label');
         taskLabel.classList.add('mx-2');
@@ -38,20 +41,26 @@ document.addEventListener('DOMContentLoaded', function () {
         const deleteButton = document.createElement('button');
         deleteButton.classList.add('fa-solid', 'fa-trash', 'mx-2');
 
+        const sepLine = document.createElement('hr');
+        sepLine.classList.add('border-1', 'border-gray-400', 'mt-2');
+
         taskWrapper.appendChild(newCheckbox);
         taskWrapper.appendChild(taskLabel);
         taskWrapper.appendChild(deleteButton);
         taskContainer.appendChild(taskWrapper);
-
+        taskContainer.appendChild(sepLine);
 
         newCheckbox.addEventListener('click', () => {
             taskLabel.classList.toggle('line-through');
+            // TODO: Move all new tasks into an array. Move done tasks into another array. Display the tasks depending in which array they are.
+            doneTasks.push(newCheckbox);
+            console.log(doneTasks);
         });
 
         deleteButton.addEventListener('click', () => {
             taskWrapper.remove();
-        })
-
-        
+            sepLine.remove();
+            taskId--;
+        });
     }
 });
